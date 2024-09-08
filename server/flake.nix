470459e506134e48dc0f1fb83a66e9a62f74f7f0
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-09-07 22:00:41 -0400

    I gravity-defied that sucker!

--- server/flake.nix
+++ server/flake.nix
@@ -10,32 +10,54 @@
     };
   };
 
-  outputs = { ... }@inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-    let 
+  outputs = {...} @ inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system: let
       config = {
         allowUnfree = true;
         android_sdk.accept_license = true;
       };
       pkgs = import inputs.nixpkgs {inherit system config;};
-      androidSdk = pkgs.androidenv.androidPkgs.androidsdk;
-    in 
-    {
+
+      buildToolsVersion = "34.0.0";
+      ndkVersion = "25.1.8937393";
+
+      androidPackages = pkgs.androidenv.composeAndroidPackages {
+        cmdLineToolsVersion = "8.0";
+        toolsVersion = "26.1.1";
+        platformToolsVersion = "34.0.4";
+        buildToolsVersions = [buildToolsVersion];
+        includeEmulator = false;
+        emulatorVersion = "30.3.4";
+        platformVersions = ["34"];
+        includeSources = false;
+        includeSystemImages = false;
+        systemImageTypes = ["google_apis_playstore"];
+        abiVersions = ["armeabi-v7a" "arm64-v8a"];
+        cmakeVersions = ["3.10.2" "3.22.1"];
+        includeNDK = false;
+        ndkVersions = [ndkVersion];
+        useGoogleAPIs = false;
+        useGoogleTVAddOns = false;
+        includeExtras = ["extras;google;gcm"];
+      };
+      androidSdk = androidPackages.androidsdk;
+    in {
       packages = rec {
         scrcpy = inputs.gradle2nix.builders.${system}.buildGradlePackage {
           pname = "scrcpy";
           version = "1.0";
           src = ./.;
           lockFile = ./gradle.lock;
-          extraGradleFlags = [ "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/34.0.0/aapt2" ];
+          extraGradleFlags = ["-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/34.0.0/aapt2"];
           ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
           # gradleInstallFlags = [ "installDist" ];
         };
         default = scrcpy;
       };
       devShells = rec {
-        gradle = pkgs.callPackage ./shell.nix {};
+        gradle = pkgs.callPackage ./shell.nix {inherit androidSdk;};
         default = gradle;
       };
     });
 }
+
